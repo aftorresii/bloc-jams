9e7eccd30c47de240578a5fed4d3c5d28b17364e
aftorresii <aftorresii@gmail.com>
2016-12-15 23:16:34 -0800

    fixed table in albums.html fixed image link albums.js

--- scripts/album.js
+++ scripts/album.js
@@ -3,28 +3,41 @@ var albumPicasso = {
     artist: 'Pablo Picasso',
     label: 'Cubism',
     year: '1881',
-    albumArtUrl: 'assers/images/albu_covers/01.png',
+    albumArtUrl: 'assets/images/album_covers/01.png',
     songs: [
         { title: 'Blue', duration: '4:26' },
         { title: 'Green', duration: '3:14' },
         { title: 'Red', duration: '5:01' },
          { title: 'Pink', duration: '3:21'},
-         { title: 'Magenta', duration: '2:15'}
+         { title: 'Magenta', duration: '2:15'},
     ]
 };
 
 var albumMarconi = {
     title: 'The Telephone',
     artist: 'Guglielmo Marconi',
-    label: 'EM'
+    label: 'EM',
     year: '1909',
-    albumArtUrl: 'assets/images/album_covers/20.png'
+    albumArtUrl: 'assets/images/album_covers/20.png',
     songs: [
         { title: 'Hello, Operator?', duration: '1:01'},
         { title: 'Ring, ring, ring', duration: '5:01'},
         { title: 'Can you hear me now?', duration: '3:14'},
-        { title: 'Wrong phone number', duration: '2:15'}
+        { title: 'Wrong phone number', duration: '2:15'},
     ]
+};
+
+var albumHoliday = {
+    title: 'Holiday Favorites',
+    artist: 'Various',
+    label: 'NP',
+    year: '2004',
+    albumArtUrl: 'assets/images/album_covers/Tree.jpg',
+    songs: [
+        { title: 'Happy Holidays', duration: '4:32'},
+        { title: 'The Holly and the Ivy', duration: '2:15'},
+        { title: 'O Christmas Tree', duration: '3:00'},   
+    ],
 };
 
 var createSongRow = function(songNumber, songName, songLength) {
@@ -39,13 +52,14 @@ var createSongRow = function(songNumber, songName, songLength) {
     return template;
 };
 
+var albumTitle = document.getElementsByClassName('album-view-title')[0];
+var albumArtist = document.getElementsByClassName('album-view-artist')[0];
+var albumReleaseInfo = document.getElementsByClassName('album-view-release-info')[0];
+var albumImage = document.getElementsByClassName('album-cover-art')[0];
+var albumSongList = document.getElementsByClassName('album-view-song-list')[0];
+
 var setCurrentAlbum = function(album) {
-     var albumTitle = document.getElementsByClassName('album-view-title')[0];
-     var albumArtist = document.getElementsByClassName('album-view-artist')[0];
-     var albumReleaseInfo = document.getElementsByClassName('album-view-release-info')[0];
-     var albumImage = document.getElementsByClassName('album-cover-art')[0];
-     var albumSongList = document.getElementsByClassName('album-view-song-list')[0];
-    
+     
      albumTitle.firstChild.nodeValue = album.title;
      albumArtist.firstChild.nodeValue = album.artist;
      albumReleaseInfo.firstChild.nodeValue = album.year + ' ' + album.label;
@@ -57,8 +71,17 @@ var setCurrentAlbum = function(album) {
          albumSongList.innerHTML += createSongRow(i + 1, album.songs[i].title, album.songs[i].duration);
      }
  };
+
+
  
  window.onload = function() {
      setCurrentAlbum(albumPicasso);
+     var albums = [albumPicasso, albumMarconi, albumHoliday];
+     var index = 1;
+     
+     albumImage.addEventListener("click", function(event) {
+        setCurrentAlbum(albums[index]);
+        index = (index + 1) % albums.length;
+    });
  };
-}
+
